2ad834714218c175176b59ff0faa5a580b515ee3
ntut-xuan <t109590031@ntut.org.tw>
2022-07-12 21:46:14 +0800

    Update Makefile

--- test.py
+++ test.py
@@ -2,4 +2,15 @@ import os
 
 status = os.system('systemctl is-active --quiet nuoj')
 print(status)
-exit(status)
+if status != 0:
+    exit(status)
+
+status = os.system('systemctl is-active --quiet nuoj-sandbox')
+print(status)
+if status != 0:
+    exit(status)
+
+status = os.system('systemctl is-active --quiet nuoj-database')
+print(status)
+if status != 0:
+    exit(status)
