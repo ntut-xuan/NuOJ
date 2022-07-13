63f0497954809d6c585deab3ae6073495d904323
ntut-xuan <t109590031@ntut.org.tw>
2022-07-13 08:16:23 +0800

    add 22.04 workflows, edit test.py

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import os
+import sys
 
 status = os.system('systemctl is-active --quiet nuoj')
 print(status)
 if status != 0:
-    exit(status)
+    sys.exit(status)
 
 status = os.system('systemctl is-active --quiet nuoj-sandbox')
 print(status)
 if status != 0:
-    exit(status)
+    sys.exit(status)
 
 status = os.system('systemctl is-active --quiet nuoj-database')
 print(status)
 if status != 0:
-    exit(status)
+    sys.exit(status)
