22b5c9f7dcea5511bcb7494537b09a6181ef2464
ntut-xuan <t109590031@ntut.org.tw>
2022-07-27 03:35:47 +0800

    fix Makefile and test.py

--- test.py
+++ test.py
@@ -10,8 +10,3 @@ status = os.system('systemctl is-active --quiet nuoj-sandbox')
 print(status)
 if status != 0:
     sys.exit(1)
-
-status = os.system('systemctl is-active --quiet nuoj-database')
-print(status)
-if status != 0:
-    sys.exit(1)
