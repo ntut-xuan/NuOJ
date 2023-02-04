c63d9c13d6cb251238629ed6f9dc7930660f68aa
ntut-xuan <t109590031@ntut.org.tw>
2023-02-04 16:34:46 +0000

    Change database from integer uid to string uid

--- python/models/__init__.py
+++ python/models/__init__.py
@@ -1,7 +1,7 @@
 from database import db
 
 class User(db.Model):
-    user_uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_uid = db.Column(db.String(36), primary_key=True)
     handle = db.Column(db.String(32), unique=True)
     password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(100), nullable=False)
@@ -9,3 +9,14 @@ class User(db.Model):
     email_verified = db.Column(db.Integer, nullable=False)
     
     __table_args__ = (db.CheckConstraint(email_verified.in_({0, 1})),)
+
+
+class Profile(db.Model):
+    user_uid = db.Column(
+        db.ForeignKey(User.user_uid, ondelete="CASCADE", onupdate="CASCADE"),
+        primary_key=True,
+    )
+    img_type = db.Column(db.String(5), default=None)
+    email = db.Column(db.String(100), default=None)
+    school = db.Column(db.String(100), default=None)
+    bio = db.Column(db.String(100), default=None)
