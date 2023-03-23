c7a307bcbd11df64a01a9dae4ea4a6f3a893d154
ponywen <pony076152340@gmail.com>
2023-03-23 14:39:15 +0000

    submission page without api

--- src/js/submission_list.jsx
+++ src/js/submission_list.jsx
@@ -1,49 +1,102 @@
-const Table = ({table_heads, table_cols})=>{
-    console.log(table_heads)
+const ConfigPayload = ({lable = '', type = 'text', width = undefined, className = '', href_pattern = undefined, replacement =[]}) => 
+    ({lable: lable,type: type, width: width, className: className,href_pattern:href_pattern ,replacement:replacement})
+
+const construct_conf = (conf) => {
+    var Conf = {}
+    var ids = []
+    conf.forEach(e => {
+        Conf[e.id] = ConfigPayload(e)
+        ids.push(e.id);
+    });
+    return [Conf, ids]
+}
+
+const Text = ({id, data, config}) => (<span>{data[id]}</span>)
+const Link = ({id, data, config}) => {
+    if (config.href_pattern === undefined) return(
+        <a href={''}>{data[id]}</a>
+    )
+    const args = config.replacement
+    const href = config.href_pattern.replace(/{(\d+)}/g, function(match, number) { 
+        return args[number] != 'undefined' ? data[args[number]] : match;
+    })
+    return(
+        <a href={href}>{data[id]}</a>
+    )
+}
+
+const Containers = ({id, data, config})=>{
+    console.log(config)
+    const containers = {
+        text: (<Text id={id} data={data} config={config}></Text>),
+        link: (<Link id={id} data={data} config={config}></Link>)
+    }
+    return containers[config.type]
+}
+
+const Column = ({ids, config, data}) =>{
+    return(
+        <tr>
+            {ids.map(id=>{
+                const element_config = config[id]
+                return(<td className={element_config.className}>
+                    <Containers id={id} data={data} config={element_config}></Containers>
+                </td>)})}
+        </tr>
+    )
+}
+
+const Table = ({raw_conf, data})=>{
+    const [config, ids] = construct_conf(raw_conf)
     return(
         <div className="overflow-hidden">
             <table className="w-full text-lg text-black dark:text-gray-400 text-center relative table-auto whitespace-nowrap leading-normal">
                 <thead>
                     <tr className="bg-orange-200">
-                        {
-                            table_heads.map(head=>
-                                <th scope="col" className={`py-3 ${head.w?`w-[${head.w}%]`:''}`}>{head.lable}</th>
-                            )
-                        }
+                    {
+                        ids.map(id=>{
+                            const col_conf = config[id]
+                            return(<th className={`px-6 py-3`} style={col_conf.width === undefined?{}:{width: col_conf.width}}>{col_conf.lable}</th>)
+                        })
+                    }
                     </tr>
                 </thead>
                 <tbody>
-                    {table_cols.map(col=>{
-                        <tr>
-                            {
-                                <td></td>
-                            }
-                        </tr>
-                    })}
+                    {data.map(row=>Column({ids: ids,config: config,data: row}))}
                 </tbody>
             </table>
         </div>
     )
 }
 
+PROBLEMAPIURL = ''
+
 const Submission_list = ()=>{
     const [column,setCol] = React.useState([])
     
-    // React.useEffect(
-        
-    // )
-    const heads = [
-        {lable: '提交ID'}, 
-        {lable: '提交時間'}, 
-        {lable: '題目'}, 
-        {lable: '提交人'},
-        {lable: '狀態'},
-        {lable: '記憶體'},
-        {lable: '時長'}
-    ]
-    
+    React.useEffect(()=>{
+        /* $.ajax({
+            url: PROBLEMAPIURL,
+            method: 'GET',
+            dataType: 'json',
+            success: (data, status, xml)=>{
+                setCol(data.list)
+            }
+        }) */
+    },[])
+
     return(
-        <Table table_heads={heads} table_cols={column}></Table>
+        <Table raw_conf={[
+            {lable: '提交ID', id: 'id', width: '6rem'},
+            {lable: '提交時間', id: 'submit_t', width: '18rem'},
+            {lable: '題目', width: '18rem', id: 'problem',type: 'link', href_pattern:'/problem/{0}', replacement: ['problem_id'] },
+            {lable: '提交人', id: 'submitter',type: 'link', href_pattern: '/profile/{0}', replacement: ['submitter']},
+            {lable: '狀態', id: 'status'},
+            {lable: '記憶體', id: 'memory'},
+            {lable: '時長', id: 'consume_time'}
+        ]} data={[
+            {id: 1, submit_t: "YYYY-MM-DDThh:mm:ss", problem_id: 1, problem:'test', submitter: 'qwe', status: 'AC', memory: '123M', consume_time: '00:00'}
+        ]}></Table>
     )
 }
 
--- static/js/submission_list.js
+++ static/js/submission_list.js
@@ -1,45 +1,137 @@
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var Table = function Table(_ref) {
-    var table_heads = _ref.table_heads,
-        table_cols = _ref.table_cols;
+var ConfigPayload = function ConfigPayload(_ref) {
+    var _ref$lable = _ref.lable,
+        lable = _ref$lable === undefined ? '' : _ref$lable,
+        _ref$type = _ref.type,
+        type = _ref$type === undefined ? 'text' : _ref$type,
+        _ref$width = _ref.width,
+        width = _ref$width === undefined ? undefined : _ref$width,
+        _ref$className = _ref.className,
+        className = _ref$className === undefined ? '' : _ref$className,
+        _ref$href_pattern = _ref.href_pattern,
+        href_pattern = _ref$href_pattern === undefined ? undefined : _ref$href_pattern,
+        _ref$replacement = _ref.replacement,
+        replacement = _ref$replacement === undefined ? [] : _ref$replacement;
+    return { lable: lable, type: type, width: width, className: className, href_pattern: href_pattern, replacement: replacement };
+};
 
-    console.log(table_heads);
+var construct_conf = function construct_conf(conf) {
+    var Conf = {};
+    var ids = [];
+    conf.forEach(function (e) {
+        Conf[e.id] = ConfigPayload(e);
+        ids.push(e.id);
+    });
+    return [Conf, ids];
+};
+
+var Text = function Text(_ref2) {
+    var id = _ref2.id,
+        data = _ref2.data,
+        config = _ref2.config;
     return React.createElement(
-        "div",
-        { className: "overflow-hidden" },
+        'span',
+        null,
+        data[id]
+    );
+};
+var Link = function Link(_ref3) {
+    var id = _ref3.id,
+        data = _ref3.data,
+        config = _ref3.config;
+
+    if (config.href_pattern === undefined) return React.createElement(
+        'a',
+        { href: '' },
+        data[id]
+    );
+    var args = config.replacement;
+    var href = config.href_pattern.replace(/{(\d+)}/g, function (match, number) {
+        return args[number] != 'undefined' ? data[args[number]] : match;
+    });
+    return React.createElement(
+        'a',
+        { href: href },
+        data[id]
+    );
+};
+
+var Containers = function Containers(_ref4) {
+    var id = _ref4.id,
+        data = _ref4.data,
+        config = _ref4.config;
+
+    console.log(config);
+    var containers = {
+        text: React.createElement(Text, { id: id, data: data, config: config }),
+        link: React.createElement(Link, { id: id, data: data, config: config })
+    };
+    return containers[config.type];
+};
+
+var Column = function Column(_ref5) {
+    var ids = _ref5.ids,
+        config = _ref5.config,
+        data = _ref5.data;
+
+    return React.createElement(
+        'tr',
+        null,
+        ids.map(function (id) {
+            var element_config = config[id];
+            return React.createElement(
+                'td',
+                { className: element_config.className },
+                React.createElement(Containers, { id: id, data: data, config: element_config })
+            );
+        })
+    );
+};
+
+var Table = function Table(_ref6) {
+    var raw_conf = _ref6.raw_conf,
+        data = _ref6.data;
+
+    var _construct_conf = construct_conf(raw_conf),
+        _construct_conf2 = _slicedToArray(_construct_conf, 2),
+        config = _construct_conf2[0],
+        ids = _construct_conf2[1];
+
+    return React.createElement(
+        'div',
+        { className: 'overflow-hidden' },
         React.createElement(
-            "table",
-            { className: "w-full text-lg text-black dark:text-gray-400 text-center relative table-auto whitespace-nowrap leading-normal" },
+            'table',
+            { className: 'w-full text-lg text-black dark:text-gray-400 text-center relative table-auto whitespace-nowrap leading-normal' },
             React.createElement(
-                "thead",
+                'thead',
                 null,
                 React.createElement(
-                    "tr",
-                    { className: "bg-orange-200" },
-                    table_heads.map(function (head) {
+                    'tr',
+                    { className: 'bg-orange-200' },
+                    ids.map(function (id) {
+                        var col_conf = config[id];
                         return React.createElement(
-                            "th",
-                            { scope: "col", className: "py-3 " + (head.w ? "w-[" + head.w + "%]" : '') },
-                            head.lable
+                            'th',
+                            { className: 'px-6 py-3', style: col_conf.width === undefined ? {} : { width: col_conf.width } },
+                            col_conf.lable
                         );
                     })
                 )
             ),
             React.createElement(
-                "tbody",
+                'tbody',
                 null,
-                table_cols.map(function (col) {
-                    React.createElement(
-                        "tr",
-                        null,
-                        React.createElement("td", null)
-                    );
+                data.map(function (row) {
+                    return Column({ ids: ids, config: config, data: row });
                 })
             )
         )
     );
 };
+
+PROBLEMAPIURL = '';
 
 var Submission_list = function Submission_list() {
     var _React$useState = React.useState([]),
@@ -47,14 +139,18 @@ var Submission_list = function Submission_list() {
         column = _React$useState2[0],
         setCol = _React$useState2[1];
 
-    // React.useEffect(
+    React.useEffect(function () {
+        /* $.ajax({
+            url: PROBLEMAPIURL,
+            method: 'GET',
+            dataType: 'json',
+            success: (data, status, xml)=>{
+                setCol(data.list)
+            }
+        }) */
+    }, []);
 
-    // )
-
-
-    var heads = [{ lable: '提交ID' }, { lable: '提交時間' }, { lable: '題目' }, { lable: '提交人' }, { lable: '狀態' }, { lable: '記憶體' }, { lable: '時長' }];
-
-    return React.createElement(Table, { table_heads: heads, table_cols: column });
+    return React.createElement(Table, { raw_conf: [{ lable: '提交ID', id: 'id', width: '6rem' }, { lable: '提交時間', id: 'submit_t', width: '18rem' }, { lable: '題目', width: '18rem', id: 'problem', type: 'link', href_pattern: '/problem/{0}', replacement: ['problem_id'] }, { lable: '提交人', id: 'submitter', type: 'link', href_pattern: '/profile/{0}', replacement: ['submitter'] }, { lable: '狀態', id: 'status' }, { lable: '記憶體', id: 'memory' }, { lable: '時長', id: 'consume_time' }], data: [{ id: 1, submit_t: "YYYY-MM-DDThh:mm:ss", problem_id: 1, problem: 'test', submitter: 'qwe', status: 'AC', memory: '123M', consume_time: '00:00' }] });
 };
 
 ReactDOM.createRoot(document.getElementById("table")).render(React.createElement(Submission_list, null));
