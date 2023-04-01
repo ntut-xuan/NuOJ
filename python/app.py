4e566077469b9ab7f4f5ba0918cfeccfb67667b7
ponywen <pony076152340@gmail.com>
2023-04-01 15:44:03 +0800

    revert app.py content

--- python/app.py
+++ python/app.py
@@ -39,7 +39,6 @@ def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
     app.register_blueprint(auth_page_bp)
     app.register_blueprint(page)
     app.register_blueprint(problem_page_bp)
-    app.register_blueprint(submission_page_bp)
 
     @app.route("/static/<path:path>")
     def fetch_static_file_from_specific_path(path):
