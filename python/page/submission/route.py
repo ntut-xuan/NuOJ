e950982b7352ec63eadf59b88e19d986d5e5f6d8
ponywen <pony076152340@gmail.com>
2023-04-06 03:29:18 +0000

    Fix template name

--- python/page/submission/route.py
+++ python/page/submission/route.py
@@ -5,4 +5,4 @@ submission_page_bp = Blueprint("submission_page", __name__)
 
 @submission_page_bp.route("/submission", methods=["GET"])
 def fetch_problem_list_page_route():
-    return render_template("problem_list.html", **locals())
+    return render_template("submission_list.html", **locals())
