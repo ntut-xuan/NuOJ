dbd41aec8578f6d27adcbf26c3a7e215078a895e
Uriah Xuan <t109590031@ntut.org.tw>
2023-06-02 17:18:07 +0800

    Add `profile` page route (#86)
    * Add profile page route
    
    * Register the profile page route
    
    * Revert mispaste and register the route
    
    * add __init__.py
    
    * Remove unused function or class
    
    * Add parameter in fetch_profile_page_route
    
    * Add tests

---
+++ python/page/profile/route.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+profile_page_bp = Blueprint("profile_page", __name__)
+
+
+@profile_page_bp.route("/profile/<string:name>", methods=["GET"])
+def fetch_profile_page_route(name: str):
+    return render_template("profile.html", **locals())
