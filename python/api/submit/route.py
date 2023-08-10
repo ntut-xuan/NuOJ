bab0a92b87ca2acd185b7b110a60cd7c1ce070a1
ntut-xuan <t109590031@ntut.org.tw>
2023-08-10 16:53:35 +0000

    Fix bugs & apply testcase into submit route

--- python/api/submit/route.py
+++ python/api/submit/route.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any
 
 import requests
@@ -7,12 +8,12 @@ from flask import Blueprint, make_response, request
 from api.auth.auth_util import get_user_by_jwt_token
 from storage.util import TunnelCode, read_file
 from database import db
-from models import Language, User, ProblemChecker, ProblemSolution, Submission
+from models import Language, User, ProblemChecker, Problem, ProblemSolution, Submission, Testcase
 
 submit_bp = Blueprint("submit", __name__, url_prefix="/api/submit")
 
 
-def get_judge_payload(user_code: str, user_code_language: str, solution: str, solution_language: str, checker: str, checker_language: str, submission_id: str):
+def get_judge_payload(user_code: str, user_code_language: str, solution: str, solution_language: str, checker: str, checker_language: str, testcase: str, submission_id: str):
     judge_payload: dict[str, Any] = {
         "user_code": {
             "code": user_code,
@@ -26,6 +27,7 @@ def get_judge_payload(user_code: str, user_code_language: str, solution: str, so
             "code": checker,
             "compiler": checker_language
         },
+        "testcase": testcase,
         "type": "Judge",
         "options": {
             "threading": True,
@@ -57,9 +59,14 @@ def submit_route():
     db.session.flush()
 
     submission_id = submission.id
-    solution, solution_language = fetch_solution_from_problem_id(problem_id)
-    checker, checker_language = fetch_checker_from_problem_id(problem_id)
-    payload = get_judge_payload(payload["code"], payload["language"], solution, solution_language, checker, checker_language, submission_id)
+    problem: Problem | None = Problem.query.filter_by(problem_id=problem_id).first()
+    assert problem is not None
+
+    solution, solution_language = _fetch_solution_from_solution_id(problem.problem_solution)
+    checker, checker_language = _fetch_checker_from_checker_id(problem.problem_checker)
+    testcase = _fetch_testcase_from_testcase_id(problem.problem_testcase)
+
+    payload = get_judge_payload(payload["code"], payload["language"], solution, solution_language, checker, checker_language, testcase, submission_id)
     
     response = requests.post("http://sandbox:4439/judge", json=payload)
     response_payload = response.json()
@@ -71,8 +78,20 @@ def submit_route():
     return make_response({"message": "OK"})
 
 
-def fetch_solution_from_problem_id(problem_id: int) -> tuple[str, str]:
-    problem_solution: ProblemSolution | None = ProblemSolution.query.filter_by(problem_id=problem_id).first()
+def _fetch_testcase_from_testcase_id(testcase_id: int) -> list[str]:
+    problem_testcase: Testcase | None = Testcase.query.filter_by(id=testcase_id)
+    
+    if problem_testcase is None:
+        return []
+    
+    filename: str = problem_testcase.filename
+    json_text: str = read_file(f"{filename}.json", TunnelCode.TESTCASE)
+    testcase: list[str] = json.loads(json_text)
+    return testcase
+
+
+def _fetch_solution_from_solution_id(solution_id: int) -> tuple[str, str]:
+    problem_solution: ProblemSolution | None = ProblemSolution.query.filter_by(id=solution_id).first()
     
     if problem_solution is None:
         return ("", "")
@@ -87,8 +106,8 @@ def fetch_solution_from_problem_id(problem_id: int) -> tuple[str, str]:
     return (content, language_name)
 
 
-def fetch_checker_from_problem_id(problem_id: int) -> tuple[str, str]:
-    problem_checker: ProblemChecker | None = ProblemChecker.query.filter_by(problem_id=problem_id).first()
+def _fetch_checker_from_checker_id(checker_id: int) -> tuple[str, str]:
+    problem_checker: ProblemChecker | None = ProblemChecker.query.filter_by(id=checker_id).first()
     
     if problem_checker is None:
         return ("", "")
