58675178dc946babf8566e159f34ecef61f62306
ntut-xuan <t109590031@ntut.org.tw>
2023-08-22 16:51:17 +0000

    Apply CRE status to submission/result route

--- python/test/submission/test_route.py
+++ python/test/submission/test_route.py
@@ -50,16 +50,25 @@ def mle_payload():
 def re_payload():
     with open("./test/submission/response/re_response.json", "r") as file:
         return json.loads(file.read())
-    
+
+
 @pytest.fixture
 def ce_payload():
     with open("./test/submission/response/ce_response.json", "r") as file:
         return json.loads(file.read())
 
+
 @pytest.fixture
 def cmle_payload():
     with open("./test/submission/response/cmle_response.json", "r") as file:
         return json.loads(file.read())
+    
+
+@pytest.fixture
+def cre_payload():
+    with open("./test/submission/response/cre_response.json", "r") as file:
+        return json.loads(file.read())
+
 
 @pytest.fixture
 def code():
@@ -363,6 +372,32 @@ class TestAddJudgeResult:
             assert (
                 verdict_error_comment.message
                 == "The programming has reached the memory limit. (9656KB)"
+            )
+
+    def test_with_cre_valid_payload_should_store_correct_data_to_database(
+        self,
+        app: Flask,
+        logged_in_client: FlaskClient,
+        cre_payload: dict[str, Any],
+        setup_submission: str
+    ):
+        response: TestResponse = logged_in_client.post(
+            "/api/submission/1/result", json=cre_payload
+        )
+
+        assert response.status_code == HTTPStatus.OK
+        with app.app_context():
+            tracker_uid: str = setup_submission
+            verdict: Verdict = Verdict.query.filter_by(tracker_uid=tracker_uid).first()
+            assert verdict.verdict == "CRE"
+            assert verdict.error_id == 1
+            verdict_error_comment: VerdictErrorComment = (
+                VerdictErrorComment.query.filter_by(id=1).first()
+            )
+            assert verdict_error_comment.failed_testcase_index == 0
+            assert (
+                verdict_error_comment.message
+                == "The programming return exitsig 6"
             )
 
     def test_with_valid_payload_should_store_file_to_storage(
